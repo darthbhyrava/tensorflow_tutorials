e0cb6e771a033e1b8cdbc4c6780f2fc2565961ab
darthbhyrava <hbhyrava@gmail.com>
2017-12-09 16:51:11 +0530

    At 2-20

--- 2_constants.py
+++ 2_constants.py
@@ -10,10 +10,25 @@ b = tf.constant([[0,1],[2,3]], name='b')
 x = tf.add(a, b, name='add')
 y = tf.multiply(a, b, name='mul')
 
+# Constants as sequences:
+# tf.linspace(start, stop, num, name=None)
+p1 = tf.linspace(1.0, 10.0, 5, name="J1")
+# tf.range(start, limit, delta)
+p2 = tf.range(3, 18, 0.5)
+# tf.range(limit)
+p3 = tf.range(6)
+
+# Randomly Generated Constants
+# s1 = tf.random_normal(shape, mean=0.0, stddev=1.0, dtype=tf.float32, seed=None, name=None)
+s1 = tf.random_normal([1,2,3])
+# s2 = tf.truncated_normal(shape, mean=0.0, stddev=1.0, dtype=tf.float32, seed=None, name=None)
+s2 = tf.truncated_normal([1,2,3])
+
+
 with tf.Session() as sess:
 	#activating Tensorboard
 	writer = tf.summary.FileWriter('./graphs', sess.graph)
-	x,y = sess.run([x, y])
-	print x, y
+	p1, p2, p3 = sess.run([p1, p2, p3])
+	print p1, p2, p3
 
 writer.close()
--- 2_values.py
+++ 2_values.py
@@ -3,3 +3,24 @@ import tensorflow as tf
 
 #filter out warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+val = [[1,2],[2,3],[4,5]]
+
+# tf.zeros(shape, dtype=tf.float32, name=None)
+# creates a tensor of shape and all elements will be zeroes(when run in a session)'
+a = tf.zeros([2,3], tf.int32)
+# tf.zeros_like(input_tensor, dtype=None, name=None, optimize=True)
+# creates a tensor of shape and type as inp_t, but all elements are 0
+b = tf.zeros_like(val)
+# same for ones
+c = tf.ones([2,3], tf.int32)
+d = tf.ones_like(val)
+
+# tf.fill(dims, value, name=None)
+# creates a tensor filled with a scalar value
+e = tf.fill([3,4], 5.5)
+
+with tf.Session() as sess:
+	writer = tf.summary.FileWriter('./graphs',sess.graph)
+	e = sess.run(e)
+	print e
